7e3292bbcb9a57ccae3eda288abfbbc5a6480c1a
brij05 <108320592+brij05@users.noreply.github.com>
2025-12-29 19:32:28 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://brijrajsolanki4_db_user:Brijraj12121@cluster1.368vou6.mongodb.net/"
-    );
+    await mongoose.connect(Mongo_URL);
     console.log("connected to mongoDB successfully");
   } catch (error) {
     console.log(error, "error while connection to db");
